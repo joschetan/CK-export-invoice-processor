618207c5ba5f631f213ff71756fe555218d3dc10
joschetan <joschetan@gmail.com>
2026-08-19 11:17:47 +0530

    Delete run_app.py

--- run_app.py
+++
@@ -1,7 +0,0 @@
-import os
-import streamlit.web.cli as stcli
-import sys
-
-if __name__ == "__main__":
-    sys.argv = ["streamlit", "run", "app.py", "--global.developmentMode=false"]
-    sys.exit(stcli.main())
